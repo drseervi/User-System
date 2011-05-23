94bbb298a0beedc13879cca31118cd72c73090e7
Felipe Santos <felipedems@gmail.com>
2011-05-23 12:20:29 -0300

    Small update

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/personaldancers/';
+$config['base_url']	= 'http://usersystem.pagodabox.com/';
 
 /*
 |--------------------------------------------------------------------------
